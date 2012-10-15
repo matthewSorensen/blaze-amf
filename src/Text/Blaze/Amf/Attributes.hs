212124ee1ad2a7ad806eb18682eee7b199de9df6
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2012-10-14 23:52:07 -0700

    Add restricted enum types for profile and unit

--- src/Text/Blaze/Amf/Attributes.hs
+++ src/Text/Blaze/Amf/Attributes.hs
@@ -3,8 +3,40 @@ module Text.Blaze.Amf.Attributes where
 
 import Text.Blaze.Amf.Template
 import Text.Blaze.Internal
+import Text.Blaze
 import Language.Haskell.TH
 
 $(attributeAs "id" "id_")
 $(attributeAs "type" "type_")
-$(attributes ["unit","profile","materialid","width","height","depth","objectid"])
+$(attributes ["materialid","width","height","depth","objectid"])
+
+data ColorSpace = SRGB | AdobeRGB | WideGamutRGB | CIERGB | CIELAB | CIEXYZ
+                  deriving(Show,Eq)
+
+instance ToValue ColorSpace where
+    toValue = preEscapedToValue
+
+    preEscapedToValue SRGB = "sRGB"
+    preEscapedToValue AdobeRGB = "AdobeRGB" 
+    preEscapedToValue WideGamutRGB = "Wide-Gamut-RGB" 
+    preEscapedToValue CIERGB = "CIERGB"
+    preEscapedToValue CIELAB = "CIELAB"
+    preEscapedToValue CIEXYZ = "CIEXYZ"
+
+data Unit = Inch | Millimeter | Meter | Feet | Micron
+            deriving(Show,Eq)
+
+instance ToValue Unit where
+    toValue = preEscapedToValue
+
+    preEscapedToValue Inch = "inch"
+    preEscapedToValue Millimeter = "millimeter"
+    preEscapedToValue Meter = "meter"
+    preEscapedToValue Feet = "feet"
+    preEscapedToValue Micron = "micron"
+
+unit :: Unit -> Attribute
+unit = customAttribute "unit" . preEscapedToValue
+
+profile :: ColorSpace -> Attribute
+profile = customAttribute "profile" . preEscapedToValue
