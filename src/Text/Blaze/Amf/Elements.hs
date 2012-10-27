3218dbf574f7e0e9b1ce7ce86d7f72200d2ea298
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2012-10-27 15:43:00 -0700

    Proper top-level amf element.

--- src/Text/Blaze/Amf/Elements.hs
+++ src/Text/Blaze/Amf/Elements.hs
@@ -4,6 +4,8 @@ module Text.Blaze.Amf.Elements where
 import Text.Blaze.Amf.Template
 import Text.Blaze.Internal
 import Language.Haskell.TH
+import Text.Blaze.Amf.Attributes
+import Data.Monoid
 
 $(elements ["x","y","z"])
 $(elements ["nx","ny","nz"])
@@ -13,8 +15,12 @@ $(elements ["rx","ry","rz"])
 $(elements ["dx1","dy1","dz1"])
 $(elements ["dx2","dy2","dz2"])
 $(elements ["deltax","deltay","deltaz"])
-$(elements ["amf","object","color","mesh","vertices","vertex"])
+$(elements ["object","color","mesh","vertices","vertex"])
 $(elements ["coordinates","normal","edge","volume","triangle","metadata"])
 $(elements ["texture","material","composite","constellation","instance"])
 
+$("amf" `elementAs` "raw_amf")
 
+amf :: Unit -> Markup -> Markup
+amf u cont = unsafeByteString "<?xml version=\"1.0\"?>" `mappend` 
+             (raw_amf ! unit u $ cont)
--- src/Text/Blaze/Amf/Template.hs
+++ src/Text/Blaze/Amf/Template.hs
@@ -7,16 +7,15 @@ import Data.Char
 
 lit = return . LitE . StringL
 
-mkElement :: String -> Q [Dec]
-mkElement n = do
-  let name = mkName n
+
+elementAs :: String -> String -> Q [Dec]
+elementAs xmlName haskName = do
+  let name = mkName haskName
   t <- name `sigD` [t| Markup -> Markup  |]
-  expr <- id [e|Parent|] `appE` lit n `appE` (lit $ '<' : n) `appE` lit ("</"++n++">")
+  expr <- id [e|Parent|] `appE` lit xmlName `appE` (lit $ '<' : xmlName) `appE` lit ("</"++ xmlName ++">")
   return [t,
           FunD name [Clause [] (NormalB expr) []], 
           PragmaD $ InlineP name $ InlineSpec True True Nothing]
-
-elements = fmap concat . mapM mkElement
 
 attributeAs :: String -> String -> Q [Dec]
 attributeAs attrName haskName = do
@@ -27,10 +26,17 @@ attributeAs attrName haskName = do
           FunD name [Clause [] (NormalB $ AppE custom $ LitE $ StringL attrName) []],
           PragmaD $ InlineP name $ InlineSpec True True Nothing]
 
-attributes :: [String] -> Q [Dec]
-attributes = fmap concat . mapM mkAttr
-    where mkAttr n = attributeAs n $ map (replace . toLower) n
-          replace ' ' = '_'
+sanitize :: String -> String
+sanitize = map (replace . toLower)
+    where replace ' ' = '_'
           replace '-' = '_'
           replace x   = x
 
+asToList :: (String -> String -> Q [Dec]) -> [String] -> Q [Dec]
+asToList f = fmap concat . mapM (\s -> f s $ sanitize s)
+
+elements :: [String] -> Q [Dec]
+elements = asToList elementAs
+
+attributes ::  [String] -> Q [Dec]
+attributes = asToList attributeAs
