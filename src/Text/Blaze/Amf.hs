8a611e581437c21ab66721bf39c4064d05d8fc56
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2012-10-28 12:14:36 -0700

    Add top-level rendering things

---
+++ src/Text/Blaze/Amf.hs
@@ -0,0 +1,14 @@
+module Text.Blaze.Amf where
+
+import Text.Blaze
+import Text.Blaze.Renderer.Utf8
+import Data.ByteString.Lazy (ByteString)
+import Codec.Archive.Zip
+
+-- This really should use a larger buffer size.
+renderAsLBS :: Markup -> ByteString
+renderAsLBS = renderMarkup
+
+-- Note that this has a horridly wrong modification time.
+renderAsZIP :: String -> Markup -> ByteString
+renderAsZIP file mark = fromArchive $ addEntryToArchive (toEntry file 0 $ renderAsLBS mark) emptyArchive
